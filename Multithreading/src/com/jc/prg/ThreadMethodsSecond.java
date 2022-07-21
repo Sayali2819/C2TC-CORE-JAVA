d7c09d02ad4120468c34ed780fdf02f139a886d5
hp <hp@LAPTOP-JGJE5HEJ>
2022-07-21 18:57:50 +0530

    Commit!

---
+++ Multithreading/src/com/jc/prg/ThreadMethodsSecond.java
@@ -0,0 +1,32 @@
+//Daemon Methods of Thread
+
+package com.jc.prg;
+
+class MyTest extends Thread{    //MyTest Thread
+	
+	@Override
+	public void run() 
+	{
+		if(Thread.currentThread().isDaemon()) 
+		{
+			System.out.println("Daemon thread");
+		}
+		else 
+		{
+			System.out.println("Child thread");
+		}
+	}
+}
+
+public class ThreadMethodsSecond {
+
+	public static void main(String[] args) {  // Main Thread
+		System.out.println("I am Main thread"); 
+		System.out.println(Thread.currentThread().isDaemon()); 
+		
+		MyTest d = new MyTest();
+		d.setDaemon(true); 
+		d.start();
+		System.out.println(d.isDaemon()); 		
+	}
+}
