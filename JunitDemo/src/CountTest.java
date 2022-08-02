39737b27e06ad56ef6254cab6a877fbd3d9ec734
hp <hp@LAPTOP-JGJE5HEJ>
2022-08-02 18:40:53 +0530

    JUnit

---
+++ JunitDemo/src/CountTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CountTest {
+
+	@Test
+	void test() {
+		Testing t = new Testing();
+		int output = t.countA("kalina sakina");
+		assertEquals(4, output);
+	}
+
+}
