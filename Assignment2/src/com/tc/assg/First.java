5b4f90f47f2e34f6cb6cfcc6a351907f3ca58725
Sayali2819 <108470808+Sayali2819@users.noreply.github.com>
2022-08-12 19:30:03 +0530

    Delete Assignment2 directory

--- Assignment2/src/com/tc/assg/First.java
+++
@@ -1,7 +0,0 @@
-package com.tc.assg;
-
-public interface First {
-	void text1();
-	void text2();
-
-}
--- Assignment2/src/com/tc/assg/Second.java
+++
@@ -1,6 +0,0 @@
-package com.tc.assg;
-
-public interface Second {
-	void text3();
-	void text4();
-}
