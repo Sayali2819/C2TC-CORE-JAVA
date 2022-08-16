1437f037ba99dd787fa064d8eacfbad6f8c8a10a
Sayali2819 <108470808+Sayali2819@users.noreply.github.com>
2022-08-16 18:57:55 +0530

    Sort!

---
+++ AssignmentPrograms/sorting/SortThird.java
@@ -0,0 +1,34 @@
+package com.tns.sorting;
+
+public class SortThird {
+	public static void printArray(int arr[]) {
+		for(int i=0; i<arr.length; i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+	public static void main(String[] args) {
+		int arr[] = {34, 89, 10, 44, 90};
+		
+		//Insertion Sort
+		for(int i=1; i<arr.length; i++)
+		{
+			int current = arr[i]; 
+			int j = i-1; 
+			
+			
+			while(j>=0 && current < arr[j]) 
+			{
+				arr[j+1] = arr[j];
+				j--;
+			}
+			
+			arr[j+1] = current;
+			
+		}
+		
+		printArray(arr);
+	}
+
+}
